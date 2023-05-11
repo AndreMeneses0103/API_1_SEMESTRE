6fe02109d3f9ddf478de115871b4a4e795fe19c2
Amandavannuccic <vannucci_job@hotmail.com>
2023-05-10 21:12:59 -0300

    Add Tela_Aceite_Usuários e Exclusão de outras telas com mesmo nome

---
+++ Tela_Aceite_ Usuários.py
@@ -0,0 +1,119 @@
+
+# Definir Dicionário Json
+
+# --------------------------------------- Início Tkinter ------------------------------------------------ #
+
+# ---------------------------------------- Janela Prinicipal -------------------------------------------- #
+import customtkinter as ctk
+from tkinter import *
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("dark-blue")
+
+# Janela é o nome dado a tela criada por CTK
+janela = ctk.CTk()
+
+# Tamanho 
+janela.geometry("700x400")
+
+# Título
+janela.title("Insigth 360")
+
+# Imagem usada para icone
+janela.iconbitmap("logo-2.ico")
+
+# Ajuste de dimensões da janela desativados
+janela.resizable(False, False)
+
+
+# Identificação do usuário
+label = ctk.CTkLabel(master=janela, text="Administrador")
+label.place(x=300, y=1)
+
+# ------------------------------------------------ Frame 1 --------------------------------------------- #
+#Frame 1 = Frame Base (estética)
+
+# Frame 1 - Dimensões
+frame = ctk.CTkFrame(master=janela, width=700, height=350)
+
+#Frame 1 - Indica o principal que a frame ficará.
+frame.pack(side=RIGHT)
+
+#Criando um novo Frame
+#fr_quadro1=frame(app,borderwidth=1,relief="solid")
+#fr_quadro1.place(x=10,y=10,width=300,height=300)
+
+# Frame Wigdets 
+label = ctk.CTkLabel(master=frame, text="Solicitações de Novos Usuários")
+label.place(x=250, y=20)
+
+# ------------------------------------------------ Frame 2 ------------------------------------------- #
+#Frame 2 = Aceite de usuários
+
+# Frame 2 - Dimensões
+frame_2 = ctk.CTkFrame(master=frame, width=500, height=100)
+
+# Frame 2 - Indica o principal que a frame ficará
+frame_2.place(x=100, y=50)
+
+# Nome do Usuário 1
+label = ctk.CTkLabel(master=frame_2, text="Usuário 1")
+label.place(x=10, y=20)
+
+# Checkbox Usuário 1
+Checkbutton = ctk.CTkCheckBox(master=frame_2, text="Aceitar").place(x=200, y=20)
+Checkbutton = ctk.CTkCheckBox(master=frame_2, text="Rejeitar").place(x=300, y=20)
+
+#Nome do Usuário 2 
+label = ctk.CTkLabel(master=frame_2, text="Usuário 2")
+label.place(x=10, y=60)
+
+# Checkbox Usuário 2
+Checkbutton = ctk.CTkCheckBox(master=frame_2, text="Aceitar").place(x=200, y=60)
+Checkbutton = ctk.CTkCheckBox(master=frame_2, text="Rejeitar").place(x=300, y=60)
+
+# ------------------------------------------------ Frame 3 ------------------------------------------- #
+#Frame 3 = Redefinição de senha
+
+# Frame 3 - Dimensões
+frame_3 = ctk.CTkFrame(master=frame, width=500, height=100)
+
+# Frame 3 - Indica o principal que a frame ficará
+frame_3.place(x=100, y=200)
+
+# Redefinição de senha 
+label = ctk.CTkLabel(master=frame, text="Redefinição de Senha")
+label.place(x=280, y=170)
+
+# Integrar com Jason
+label = ctk.CTkLabel(master=frame_3, text="Usuário x")
+label.place(x=10, y=20)
+
+label = ctk.CTkEntry(master=frame_3, placeholder_text="Nova Senha", width=200).place(x=200, y=20)
+
+# Integrar com Jason
+label = ctk.CTkLabel(master=frame_3, text="Usuário y")
+label.place(x=10, y=60)
+
+label = ctk.CTkEntry(master=frame_3, placeholder_text="Nova Senha", width=200).place(x=200, y=60)
+
+# Logo Marca ***************************************************** Erro
+#from PIL import Image, ImageTk
+
+# Carrega a imagem usando a biblioteca PIL
+#img = Image.open("logo 2.png")
+#logo_img = ImageTk.PhotoImage(img)
+
+# Cria um rótulo para exibir a imagem
+#logo_label = ctk.CTkLabel(master=frame, image=logo_img)
+
+# Armazena uma referência para evitar que a imagem seja coletada pelo coletor de lixo
+#logo_label.image = logo_img  
+
+# Define a posição do rótulo na janela
+#logo_label.place(x=550, y=10)  
+
+# Botão
+Button=ctk.CTkButton(master=frame, text="MENU INICIAL", width=100, text_color=('black'), cursor='hand2', fg_color='#00FFFF', hover_color='#2FCDCD').place(x=580, y=320)
+
+janela.mainloop()
