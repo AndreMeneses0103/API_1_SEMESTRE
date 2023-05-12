33744ecb410ee498ee97e653f0029e4462f19b99
santosjhony12 <santosdesouzajhony@gmail.com>
2023-05-11 22:52:19 -0300

    Função

--- TelaBV.py
+++ TelaBV.py
@@ -119,9 +119,16 @@ def abrir():
 
 
             def AbrirAv():
-                janela.destroy()
+
+
                 
+                janela.destroy()
+                #função de abrir a avaliação
                 TelaAV.abrir_avaliacao(sprintSelecionada.get(), timeSelecionado.get(), turmaSelecionada.get())
+
+
+
+
             dashboard_button = ctk.CTkButton(master=janela, text="Dashboards", width=110, text_color='black', fg_color="#00FFFF", font = ('Roboto', 14), cursor="hand2", hover_color='#2FCDCD', command=AbrirDashboards).place(x=30, y=560)
             cadastrar_button = ctk.CTkButton(master=janela, text="Avaliação", width=110, text_color='black', fg_color="#00FFFF", font = ('Roboto', 14), cursor="hand2", hover_color='#2FCDCD', command=AbrirAv).place(x=1020, y=560)
             logout_button = ctk.CTkButton(master=janela, text="Logout", width=90, text_color='black', fg_color="#00FFFF", font = ('Roboto', 14), cursor="hand2", hover_color='#2FCDCD', command=Close).place(x=1050, y=15)
