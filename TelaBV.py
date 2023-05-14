7f6849cdd4aac7f4cf843630f4d4fb77248c9d44
santosjhony12 <santosdesouzajhony@gmail.com>
2023-05-14 14:45:07 -0300

    Organizando os Titulos para Insight 360º

--- TelaBV.py
+++ TelaBV.py
@@ -25,7 +25,7 @@ def abrir():
 
         def tela(self):    
             janela.geometry("1200x650") #DEFINO O TAMANHO DA JANELA
-            janela.title("Sistema de login")
+            janela.title("Insight 360º")
             janela.iconbitmap("logo_insight.ico")
             janela.resizable(False, False) #defino que o usuário não pode redimensionar a tela
             pass
