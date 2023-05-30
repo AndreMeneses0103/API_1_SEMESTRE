0cc64be236c1c365c081726c03b8b417d885c352
viniciusmangaba <viniciusmangaba.ti@gmail.com>
2023-05-30 09:12:11 -0300

    Adicionando função de remoção de usuário

--- Tela_Administradores.py
+++ Tela_Administradores.py
@@ -24,6 +24,34 @@ def open_menu():
 #Voltar para tela inicial
 Button=ctk.CTkButton(master=janelaADM, text="Voltar", width=120, cursor='hand2', text_color=('black'), fg_color="#5CE1E6", hover_color='#2FCDCD', font=('Roboto', 14), command=open_menu).place(x=1000, y=612)
 
+
+with open("data_json/users.json", "r") as file:
+    acesso_usuarios = json.load(file)
+
+usuarios = acesso_usuarios["usuarios"]
+
+frame_3 = ctk.CTkScrollableFrame(master=janelaADM, fg_color='#c0c0c0', width=1000, height=200)
+frame_3.place(x=100, y=300)
+
+for x in range (len(usuarios)):
+    if usuarios[x]["cargo"] == "user" and usuarios[x]["aceito"]:
+        label = ctk.CTkLabel(master=frame_3, text=usuarios[x]["id"], text_color=('black'), font=("Roboto", 20, "bold")).grid(column=0, row=x, padx=25, pady=10)
+        button_promote = ctk.CTkButton(master=frame_3, text="Tornar Administrador", width=10, cursor='hand2', text_color=('black'), fg_color="#5CE1E6", hover_color='#2FCDCD', font=('Roboto', 14)).grid(column=1, row=x, padx=50, pady=5)
+            
+        def deletar_usuarios(delete):
+            def selecao():
+                print ("1")
+
+            return selecao
+            print(delete)
+
+        button_remove = ctk.CTkButton(master=frame_3, text="Remover Usuário", width=10, cursor='hand2', text_color=('black'), fg_color="#5CE1E6", hover_color='#2FCDCD', font=('Roboto', 14), command=deletar_usuarios(x)).grid(column=2, row=x, padx=10, pady=5)
+
+    #usuario_remover = usuarios.pop(index)
+
+    with open("data_json/users.json", "w") as file:
+        json.dump(acesso_usuarios, file, indent=4)
+
 #Frame.01
 frame = ctk.CTkFrame(master=janelaADM, width=1200, height=550)
 frame.place(x=0, y=50)
